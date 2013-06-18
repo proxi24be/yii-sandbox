990bb81723092a243d4f6b4aec95c6e1aaab41a1
bluenight <novou.tran@live.fr>
2013-06-18 13:18:45 +0200

    Modification of read method, now the method accept multi parameters.

--- angularjs/prototype/model/GenericModel.js
+++ angularjs/prototype/model/GenericModel.js
@@ -3,7 +3,6 @@
  * User: bluenight
  * Date: 6/12/13
  * Time: 4:52 PM
- * To change this template use File | Settings | File Templates.
  */
 
 SetupPrototype.service('GenericModel',
@@ -14,7 +13,17 @@ SetupPrototype.service('GenericModel',
         this.read = function($http, model)
         // Model is a string.
         {
-            return $http.get(url + '/read?model=' + model);
+            // This test is used for not breaking code.
+            if (typeof model.model == 'undefined')
+                return $http.get(url + '/read?model=' + model);
+            else
+            {
+                // New way to do.
+                var param = '';
+                for (var prop in model)
+                    param = param + prop + '=' + model[prop] + '&'
+                return $http.get(url + '/read?'+ param);
+            }
         }
 
         this.create = function($http, model)
