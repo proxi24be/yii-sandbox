caf82571ba5db21b99329fa66244fd9e5499e1b9
bluenight <novou.tran@live.fr>
2013-06-21 13:04:33 +0200

    new class HttpFormParam + unit test

---
+++ protected/components/helper/HttpFormParam.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: TRANN
+ * Date: 6/21/13
+ * Time: 12:30 PM
+ */
+
+namespace application\components\helper;
+
+class HttpFormParam extends HttpParamAbstract {
+
+
+    public function convert()
+    {
+
+    }
+}
---
+++ protected/tests/unit/HttpFormParamTest.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * User: TRANN
+ * Date: 6/21/13
+ * Time: 12:44 PM
+ */
+
+class HttpFormParamTest {
+
+}
